c70799a91c80c8a0fc331b9276a8b98aa7103240
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-02-27 00:51:10 +0100

    Update SmallPersonistItem.jsx

--- src/person/SmallPersonistItem.jsx
+++ src/person/SmallPersonistItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function LargePersonistItem({person}) {
+export default function SmallPersonistItem({person}) {
     const {name,age}=person;
   return (
     <div>
