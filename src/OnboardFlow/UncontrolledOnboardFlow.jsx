a5df7ca2a72d3cc46720b5d464d720138ec210e0
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-03-02 22:34:34 +0100

    Update UncontrolledOnboardFlow.jsx

--- src/OnboardFlow/UncontrolledOnboardFlow.jsx
+++ src/OnboardFlow/UncontrolledOnboardFlow.jsx
@@ -5,8 +5,21 @@ export default function UncontrolledOnboardFlow({children,onFinsh}) {
     const [onboardData,setOnboardData]=useState({})
     const  [index,setIndex]=useState(0)
 
-  const goTonext=()=>{
-        setIndex(index+1)
+  const goTonext=(stepdata)=>{
+    const nextIndex=index+1;
+    const upadateData={
+      ...onboardData,
+      ...stepdata
+    };
+    console.log(upadateData);
+    if (nextIndex <children.length) {
+         setIndex(nextIndex)
+    }else{
+      onFinsh(upadateData)
+    }
+
+setOnboardData(upadateData)
+        
   }
     const currentChlid=React.Children.toArray(children)[index];
    if (React.isValidElement(currentChlid)) {
