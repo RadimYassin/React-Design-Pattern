139cea4d0a0c46776161a332aa02d5a4d1f07083
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-03-02 23:12:03 +0100

    Update ControlledOnboardFlow.jsx

--- src/OnboardFlow/ControlledOnboardFlow.jsx
+++ src/OnboardFlow/ControlledOnboardFlow.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function ControlledOnboardFlow() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function ControlledOnboardFlow({children,onFinsh,index,onNext}) {
+   
+
+  const goTonext=(stepdata)=>{
+ onNext(stepdata)
+        
+  }
+    const currentChlid=React.Children.toArray(children)[index];
+   if (React.isValidElement(currentChlid)) {
+              return React.cloneElement(currentChlid,{goTonext})
+   }
+     
+     return currentChlid;
+
+
 }
